811576c92c4a178c95f9313a6165b29717909480
hao1234good1234 <hao1234good1234@163.com>
2025-09-27 19:18:42 +0800

    最后一次更新

--- helloworld.py
+++ helloworld.py
@@ -11,3 +11,12 @@ print("Hello, World!")
 
 print(type(name))
 print(type(score))
+print(type(a))
+print(type(b))
+print(type(3.14))
+print(type(True))
+if score >= 60:
+    print(name, "及格了")
+else:
+    print(name, "不及格")
+print("结束")
