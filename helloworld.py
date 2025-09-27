e08d340b9f085326f7a1fa39691f67b79cfffff8
hao1234good1234 <hao1234good1234@163.com>
2025-09-27 10:46:41 +0800

    提交到git

---
+++ helloworld.py
@@ -0,0 +1,36 @@
+b = 4
+a = 5
+print(a + b)
+
+
+print("Hello, World!")
+
+print(a * b)
+
+bb = "hello python"
+age = 19 
+height = 1.75
+is_student = True
+print(age + 1)
+
+print(height + 0.25)
+print(bb + " is good")
+print("My name is", bb)
+
+print(type(age))
+
+age = 59
+print(age)
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+x, y = 5, 10
+print(x, y)
+x, y = y, x
+print(x, y)
+
+
+name = "Tom"
+score = 95 
+print(name, "的分数是", score)
