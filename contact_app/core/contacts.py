b936e96ab215de0ad6ce728a0ddb6a262b77bae8
hao1234good1234 <hao1234good1234@163.com>
2025-10-03 11:08:45 +0800

    update

--- contact_app/core/contacts.py
+++ contact_app/core/contacts.py
@@ -5,7 +5,7 @@ def add_contact(name, phone):
     contacts = load_contacts()
     contacts[name] = phone
     save_contacts(contacts)
-    print(f"✅ {name} 已添加")
+    print(f"{name} 已添加")
 
 def get_all_contacts():
     return load_contacts()
